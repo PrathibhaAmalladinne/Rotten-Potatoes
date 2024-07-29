9310d013b1e794996bbcd55ad40df89644f52a84
PrathibhaAmalladinne <prathibha2804@gmail.com>
2024-07-30 02:36:18 +0530

    RP #2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 //import StartRating from './StarRating';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 // function handleSetMovieRating({rating}){
 //   const [displayRating,setDisplayRating] = useState(0);
